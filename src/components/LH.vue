16b66864ffaa6edec509233924404a7c36eae74a
wanght001 <wanght001@cmrh.com>
2018-06-21 14:54:52 +0800

    底部card

--- src/components/LH.vue
+++ src/components/LH.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-  <div class="top">
+    <div class="top">
       <div style="margin-left: 710px" @click=home v-bind:class = "{change: flag1}"><p>Home</p></div>
       <div @click=about v-bind:class = "{change: flag2}"><p>About Me</p></div>
       <div @click=diary v-bind:class = "{change: flag3}"><p>My Diary</p></div>
@@ -16,11 +16,26 @@
           <p>Something...</p>
         </Modal>
       </div>
-  </div>
+    </div>
     <div class="body">
-     <div id="clock">
-      <p class="date">{{ date }}</p>
-      <p class="time">{{ time }}</p>
+      <div id="clock">
+        <p class="date">{{ date }}</p>
+        <p class="time">{{ time }}</p>
+      </div>
+      <div class="card">
+        <Row>
+          <Col span="11">
+            <Card dis-hover class="card1">
+              <p slot="title" class="card1-top">
+                <Icon type="ios-paper-outline" color="#FFFFFF" size=24></Icon>&nbsp;&nbsp;&nbsp;new papers here
+              </p>
+            </Card>
+          </Col>
+          <Col span="11" offset="2">
+            <Card dis-hover class="card2">
+            </Card>
+          </Col>
+        </Row>
       </div>
     </div>
   </div>
@@ -124,7 +139,7 @@ export default {
     text-align: center;
     position: absolute;
     left: 50%;
-    top: 50%;
+    top: 30%;
     -webkit-transform: translate(-50%, -50%);
     transform: translate(-50%, -50%);
     color: #daf6ff;
@@ -146,5 +161,32 @@ export default {
     background: -webkit-radial-gradient(ellipse at center, #0a2e38 0%, #000000 70%);
     background: radial-gradient(ellipse at center, #0a2e38 0%, #000000 70%);
     background-size: 100%;
+  }
+  .card{
+    position: relative;
+    top: 45%;
+  }
+  .card1{
+    padding-left: 40px;
+    padding-right: 40px;
+    height: 477px;
+    margin-left: 120px;
+    margin-right: 240px;
+    background-color: #382a27
+  }
+  .card2 {
+    height: 477px;
+    margin-left: 240px;
+    margin-right: 120px;
+    background-color: #382a27
+  }
+  .card .ivu-card-bordered{
+    border: none;
+  }
+  .card1-top{
+    height: 30px;
+    font-size: 16px;
+    color: white;
+    font-family: "Microsoft YaHei";
   }
 </style>
